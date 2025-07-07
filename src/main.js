56d79d50379eb16e97e6645d4cae7734d7e46f39
Ismet <kadyrovismet1999@gmail.com>
2025-07-07 19:54:54 +0500

    fix: fix build error

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
 
 // document.querySelector('#app').innerHTML = `
 // `
-
-setupCounter(document.querySelector('#counter'))
+//
+// setupCounter(document.querySelector('#counter'))
